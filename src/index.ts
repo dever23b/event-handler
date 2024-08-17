332cb5f386635ddf3927146bbfff80d6c153a561
Tyler Devereaux <tyler.devereaux@gmail.com>
2024-08-17 19:22:01 -0400

    initial import

---
+++ src/index.ts
@@ -0,0 +1,81 @@
+export type EventHandlerCallback<TArgs, TReturn> = (
+  ...args: TArgs[]
+) => TReturn;
+
+export interface EventHandlerCollection<
+  TCallback extends EventHandlerCallback<any, void>
+> {
+  addListener: (callback: TCallback) => void;
+  hasListener: (callback: TCallback) => void;
+  removeListener: (callback: TCallback) => void;
+}
+
+export class EventHandler<
+  TCallback extends EventHandlerCallback<any, any> = () => void
+> {
+  // #listeners = new Map<string, TCallback[]>();
+  #listeners = new Set<TCallback>();
+  #triggered?: Parameters<TCallback>;
+
+  addListener(callback: TCallback) {
+    // this.#listeners.has(event) || this.#listeners.set(event, []);
+    // this.#listeners.get(event)!.push(callback);
+    this.#listeners.add(callback);
+
+    if (this.#triggered) {
+      callback(this.#triggered);
+    }
+  }
+
+  hasListener(callback: TCallback) {
+    return this.#listeners.has(callback);
+  }
+
+  hasListeners() {
+    return this.#listeners.size > 0;
+  }
+
+  get onEvent(): EventHandlerCollection<TCallback> {
+    return {
+      addListener: this.addListener.bind(this),
+      hasListener: this.hasListener.bind(this),
+      removeListener: this.removeListener.bind(this),
+    };
+  }
+
+  removeListener(callback: TCallback | true) {
+    if (callback === true) {
+      // remove all
+      // this.#listeners.delete(event);
+      this.#listeners.clear();
+    } else {
+      // let listeners = this.#listeners.get(event);
+      // if (listeners) {
+      //   this.#listeners.set(
+      //     event,
+      //     listeners.filter((value) => !(value === callback))
+      //   );
+      // }
+      this.#listeners.delete(callback);
+    }
+  }
+
+  trigger(context?: any, ...args: Parameters<TCallback>) {
+    // this.#triggeredEvents.set(event, args);
+    // let listeners = this.#listeners.get(event);
+    // if (listeners && listeners.length) {
+    //   listeners.forEach((listener) => {
+    //     listener(...args);
+    //   });
+    // }
+
+    this.#triggered = args;
+    if (this.#listeners.size > 0) {
+      this.#listeners.forEach((callback) => {
+        callback(...args);
+      }, context);
+    }
+  }
+}
+
+export default EventHandler;
